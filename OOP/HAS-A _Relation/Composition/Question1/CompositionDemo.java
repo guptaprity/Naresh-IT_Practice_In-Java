4f47025f1c4c8f8235a5526c05947d709734f01f
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-08-23 22:01:49 +0530

    Create CompositionDemo.java

---
+++ OOP/HAS-A _Relation/Composition/Question1/CompositionDemo.java
@@ -0,0 +1,11 @@
+package com.prity.composition;
+
+public class CompositionDemo {
+
+	public static void main(String[] args) {
+		Car naxon = new Car("Naxon EV", 20250);
+		System.out.println(naxon);
+
+	}
+
+}
