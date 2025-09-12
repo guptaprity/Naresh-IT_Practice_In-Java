4b3da3369f6699dba341c049fb700a1367f7ae39
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-09-12 21:29:49 +0530

    Create Lion.java

---
+++ Java/OOP/Abstracts/Assignment2/Lion.java
@@ -0,0 +1,13 @@
+package com.abstractAssignmentTwo;
+
+class Lion extends Animal {
+	
+    @Override
+	public  void roam(){
+	  System.out.println("Lion Animal is roaming");
+	}
+	public void roar() {
+		System.out.println("Lion is roaring");
+	}
+
+}
