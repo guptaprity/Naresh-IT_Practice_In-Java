3152815d8cebd710e89c52cad31e63486800b7d1
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-09-12 20:42:09 +0530

    Create Area.java

---
+++ Java/OOP/Abstracts/Assignment1/Area.java
@@ -0,0 +1,19 @@
+package com.prity.abstracts;
+
+public class Area {
+
+	public static void main(String[] args) {
+		   Shape s1 = new Rectangle(30.3, 45.4);
+	       Shape s2 = new Circle(23.2);
+	       Shape s3 = new Triangle(146.2, 40.0);
+
+	        s1.printDetails();
+	        System.out.println();
+	        s2.printDetails();
+	        System.out.println();
+	        s3.printDetails();
+	    
+
+	}
+
+}
