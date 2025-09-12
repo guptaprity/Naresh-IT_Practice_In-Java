ee624108a8a7f02a8da56e305e917ccede993c3e
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-09-12 21:30:14 +0530

    Create Dog.java

---
+++ Java/OOP/Abstracts/Assignment2/Dog.java
@@ -0,0 +1,13 @@
+package com.abstractAssignmentTwo;
+
+public class Dog extends Animal{
+	
+	   @Override
+		public  void roam(){
+		  System.out.println("Dog Animal is roaming");
+		}
+		public void bark() {
+			System.out.println("Dog is Barking");
+		}
+
+}
