bb41c8e0dfdb81cdd81fbad043ed33e470d87e41
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-10-08 22:47:10 +0530

    Create  CarUser.java

---
+++ Java/ExceptionHandle/CostumExceptionHandling1/CarUser.java
@@ -0,0 +1,42 @@
+package com.prity.CustomException;
+
+public class CarUser {
+    public static void main(String[] args) {
+
+        try {
+            CarTest.stop("stop");
+        } catch (CarStoppedException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            CarTest.stop("go");
+        } catch (CarStoppedException e) 
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            CarTest.puncture("puncture");
+        } catch (CarPunctureException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            CarTest.puncture("move");
+        } catch (CarPunctureException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            CarTest.carHeat(60);
+        } catch (CarHeatException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            CarTest.carHeat(40);
+        } catch (CarHeatException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
